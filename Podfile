94c3de76767654c452c2e2b980dfa02fc673c6f4
Gali Srikanth <galisrikanth19@gmail.com>
2022-11-10 16:12:39 +0530

    Updated code

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SaveDataAsCache' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SaveDataAsCache
+  pod "Cache"
+
+end
--- SaveDataAsCache/ViewController.swift
+++ SaveDataAsCache/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import Cache
 
 class ViewController: UIViewController {
-
+    let cacheKey = "keyName"
+    var storage: Storage<String, User> {
+        let diskConfig = DiskConfig(name: "LocalDisk")
+        let memoryConfig = MemoryConfig(expiry: .never, countLimit: 0, totalCostLimit: 0)
+        let storage = try! Storage<String, User>(
+                  diskConfig: diskConfig,
+                  memoryConfig: memoryConfig,
+                  transformer: TransformerFactory.forCodable(ofType: User.self))
+        return storage
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func saveUserDetails(_ sender: UIButton) {
+        let userM = User(firstName: "John", lastName: "Snow")
+        do {
+            try self.storage.setObject(userM, forKey: cacheKey)
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func fetchUserDetails(_ sender: UIButton) {
+        do {
+            let userM = try self.storage.object(forKey: cacheKey)
+            print(userM.firstName ?? "")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
 
+struct User: Codable {
+  let firstName: String?
+  let lastName: String?
+}
